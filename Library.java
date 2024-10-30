7f3b9634706d75c9edb9eccd353e9ded07929868
Trang Tran <mntran@smith.edu>
2024-10-30 00:50:25 -0400

    DONE!

--- Library.java
+++ Library.java
@@ -1,70 +1,124 @@
 import java.util.Hashtable;
 import java.util.Map;
-/* This is a stub for the Library class */
+
+/**
+ * A Library contains a collection of books where each book title is associated with its availability status.
+ * The Library allows adding, removing, checking out, and returning books, as well as checking book availability.
+ */
 public class Library extends Building {
+
     private Hashtable<String, Boolean> collection;
 
+    /**
+     * Constructs a new Library object with the given name, address, and number of floors.
+     * Initializes an empty collection of books.
+     *
+     * @param name    the name of the library
+     * @param address the address of the library
+     * @param nFloors the number of floors in the library
+     */
     public Library(String name, String address, int nFloors) {
-      super(name, address, nFloors);
-      this.collection = new Hashtable<String,Boolean>();
-      System.out.println("You have built a library!");
+        super(name, address, nFloors);
+        this.collection = new Hashtable<String, Boolean>();
+        System.out.println("You have built a library!");
     }
 
-    public void addTitle(String title){
-      if(this.containsTitle(title) == true){
-        throw new RuntimeException("Title already in collection");
-      } else {
-        this.collection.put(title,true);
-      }
-    } 
-
-    public String removeTitle(String title){
-      if(this.containsTitle(title) == false){
-        throw new RuntimeException("Title not in collection. Can't be removed");
-      } else {
-        this.collection.remove(title);
-        return title;
-      }
-    }
-  
-    public void checkOut(String title){
-      if(isAvailable(title) == true) {
-        this.collection.replace(title,false);
-      } else {
-        throw new RuntimeException("Title has been borrowed and is not available to be checked out.");
-      }
-    } 
-    
-    public void returnBook(String title){
-      if(!this.collection.containsKey(title)){
-        throw new RuntimeException("Title wasn't checked out from this library. Wrong library perhaps?");
-      } else {
-        this.collection.replace(title,true);
-      }
-    } 
-
-    public boolean containsTitle(String title){
-      return this.collection.containsKey(title);
+    /**
+     * Adds a book title to the library collection if it is not already present.
+     *
+     * @param title the title of the book to add
+     * @throws RuntimeException if the title already exists in the collection
+     */
+    public void addTitle(String title) {
+        if (this.containsTitle(title)) {
+            throw new RuntimeException("Title already in collection");
+        } else {
+            this.collection.put(title, true);
+        }
     }
 
-    public boolean isAvailable(String title){
-      if (containsTitle(title) == false){
-        throw new RuntimeException("Title is not available in the library");
-      } else {
-        return this.collection.get(title);
-      }
+    /**
+     * Removes a book title from the library collection if it is present.
+     *
+     * @param title the title of the book to remove
+     * @return the title of the book that was removed
+     * @throws RuntimeException if the title is not in the collection
+     */
+    public String removeTitle(String title) {
+        if (!this.containsTitle(title)) {
+            throw new RuntimeException("Title not in collection. Can't be removed");
+        } else {
+            this.collection.remove(title);
+            return title;
+        }
     }
 
-    public void printCollection(){
+    /**
+     * Checks out a book by setting its availability status to false if the book is available.
+     *
+     * @param title the title of the book to check out
+     * @throws RuntimeException if the title is not available (already checked out)
+     */
+    public void checkOut(String title) {
+        if (isAvailable(title)) {
+            this.collection.replace(title, false);
+        } else {
+            throw new RuntimeException("Title has been borrowed and is not available to be checked out.");
+        }
+    }
+
+    /**
+     * Returns a book by setting its availability status to true if it is in the collection.
+     *
+     * @param title the title of the book to return
+     * @throws RuntimeException if the title is not in the collection
+     */
+    public void returnBook(String title) {
+        if (!this.collection.containsKey(title)) {
+            throw new RuntimeException("Title wasn't checked out from this library. Wrong library perhaps?");
+        } else {
+            this.collection.replace(title, true);
+        }
+    }
+
+    /**
+     * Checks if a given title exists in the library collection.
+     *
+     * @param title the title of the book to check
+     * @return true if the title exists in the collection; false otherwise
+     */
+    public boolean containsTitle(String title) {
+        return this.collection.containsKey(title);
+    }
+
+    /**
+     * Checks if a book is available for checkout.
+     *
+     * @param title the title of the book to check
+     * @return true if the book is available; false otherwise
+     * @throws RuntimeException if the title is not in the collection
+     */
+    public boolean isAvailable(String title) {
+        if (!containsTitle(title)) {
+            throw new RuntimeException("Title is not available in the library");
+        } else {
+            return this.collection.get(title);
+        }
+    }
+
+    /**
+     * Prints the entire collection of books in a table format showing title and availability.
+     */
+    public void printCollection() {
         // Print the header row
         System.out.printf("%-40s %-10s%n", "Book Title", "Available");
         System.out.println("---------------------------------------- ----------");
 
         // Print each book-availability status pair
         for (Map.Entry<String, Boolean> entry : this.collection.entrySet()) {
-            System.out.printf("%-40s %-10s%n", entry.getKey(), entry.getValue());    
-          }
+            System.out.printf("%-40s %-10s%n", entry.getKey(), entry.getValue());
         }
+    }
 
     public static void main(String[] args) {
       Library Neilson = new Library("Neilson","7 Neilson Drive",4);
@@ -122,6 +176,15 @@ public class Library extends Building {
         System.out.println(e.getMessage());
       }
 
+      // Check if available
+      Neilson.isAvailable("10 things I love about you");
+
+      try {
+        Neilson.isAvailable("Shoe Dog");
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+      }
+      
       // Print the collection
       Neilson.printCollection();
 
--- reflection.md
+++ reflection.md
@@ -1,4 +1,7 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+It is time efficient since I don't have to repeat each line of codes when initializing a new instances by using super(). Inheritance gives me the option to customize methods and attributes for different subclasses. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+It was super interesting to realize that the parent class of Hashtable is Dictionary. This realization makes reading the Hashtable documentation easier and quicker to locate the relevant methods.
+One thing I'm still unsure is when to throw and exception and when to sysout the error. Similarly, do I need to try catch every single line in main() or only for those lines where I'm expecting an error. 
